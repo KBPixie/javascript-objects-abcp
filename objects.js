11d0c1fcb5cb3b912176019ae9dfdc66d3b40002
Kristen Bullock <kristenbullock84@gmail.com>
2018-01-14 02:25:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,10 @@ var playlist = {
   artistName: "songTitles"
 };
 
+function updatePlaylist(playlist, artistName, songTitle){
+  
+}
+
+function removeFromPlaylist(playlist, artistName, songTitle){
+  
+}
